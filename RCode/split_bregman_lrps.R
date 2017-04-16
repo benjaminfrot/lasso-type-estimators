7217198d5bf7517bdfdb86170899b3fed1a97aae
Benjamin Frot <benjamin.frot@stat.math.ethz.ch>
2017-04-16 14:40:23 +0200

    Add better implementation of ADMM for LRPS

--- RCode/split_bregman_lrps.R
+++ RCode/split_bregman_lrps.R
@@ -1,8 +1,4 @@
-# An implementation of http://arxiv.org/abs/1110.3076
-
-library(matrixcalc)
-
-split.bregman.low.rank.plus.sparse.update.parameters <- function(ps, opts) {
+.split.bregman.low.rank.plus.sparse.update.parameters <- function(ps, opts) {
   lp1 <- opts$lp1
   lp2 <- opts$lp2
   mu <- opts$mu
@@ -11,8 +7,8 @@ split.bregman.low.rank.plus.sparse.update.parameters <- function(ps, opts) {
   S <- ps$S
   L <- ps$L
   U <- ps$U
-
-  # Update A  
+  
+  # Update A
   X1 <- mu * (S - L) - C - U
   X2 <- X1 %*% X1 + 4 * mu * diag(dim(X1)[1])
   X2 <- 0.5 * (X2 + t(X2))
@@ -49,8 +45,42 @@ split.bregman.low.rank.plus.sparse.update.parameters <- function(ps, opts) {
   ps
 }
 
-split.bregman.low.rank.plus.sparse <- function(Sigma, Lambda1, Lambda2, init=NULL, maxiter=3000, mu=0.05, tol=1e-07) {
-  
+#' Inverse covariance estimation via a low-rank plus sparse decomposition
+#'
+#' @export
+#' @details Minimise the function \deqn{-logdet S + Tr(\Sigma S) + \lambda_1 ||S||_1 + \lambda_2 ||L||_\ast}
+#' as per the estimator suggested in Chandrasekaran et. al (doi:10.1214/11-AOS949). We use Split-Bregman iterations
+#' as described by Ye et al in https://arxiv.org/abs/1110.3076.
+#' @param Sigma The sample covariance matrix
+#' @param Lambda1 Penalty on the l1-norm of the sparse component S.
+#' @param Lambda2 Penalty on the nuclear norm of the low-rank component L.
+#' @param maxiter Maximum number of iterations.
+#' @param mu Step size
+#' @param tol Tolerance. The algorithm stops when ||S_k - S_k-1||_F / ||S_k-1||_F + ||L_k - L_k-1||_F / ||L_k-1||_F < tol.
+#' @param init Initial parameters. Can be used for warm starts. See examples below.
+#' @return A list L where L$S (resp. L$L) is the estimate of the sparse matrix (resp. low-rank matrix).
+#' L$termcode is the termination code (0: good termination, -1: did not converge, -2: Lambda1 was so large that the estimated S is empty.)
+#' L$iter is the number of iterations. L$diffs is the vector containing the sequence ||S_k - S_k-1||_F / ||S_k-1||_F + ||L_k - L_k-1||_F / ||L_k-1||_F
+#' for k=1:L$iter, it shows the convergence speed of the algorithm.
+#' @examples
+#' # Generate some low-rank plus sparse data with 4 confounders
+#' set.seed(0)
+#' toy.data <- generate.conditional.data(n = 1000, 2, 5, 32)
+#' Y <- cbind(toy.data$Z, toy.data$X)
+#' Sigma = cov2cor(cov(Y))
+#' # Fit the model with gamma = 0.2
+#' gamma <- 0.2
+#' lambda <- 0.1
+#' fit <- fit.low.rank.plus.sparse(Sigma, gamma * lambda, (1-gamma)*lambda)
+#' # Look at the convergence
+#' plot(fit$diffs)
+#' # Look at the sparse matrix
+#' image(fit$S!=0)
+#' # Now, fit for another value of lambda, but reuse the previous fit for a warm start
+#' lambda <- 0.2
+#' fit <- fit.low.rank.plus.sparse(Sigma, gamma * lambda, (1-gamma)*lambda, init = fit)
+fit.low.rank.plus.sparse <- function(Sigma, Lambda1, Lambda2, init=NULL, maxiter=10000, mu=0.01, tol=1e-07) {
+  require(matrixcalc)
   # Sigma : Covariance Matrix
   # Lambda 1 : Penalty on S
   # Lambda 2 : Penalty on L
@@ -73,26 +103,33 @@ split.bregman.low.rank.plus.sparse <- function(Sigma, Lambda1, Lambda2, init=NUL
     parameters = init
   }
   
-  diffs <- c()  
+  diffs <- c()
+  parameters$termcode <- -1
   for (i in 1:maxiter) {
-    new_parameters <- split.bregman.low.rank.plus.sparse.update.parameters(parameters, options)
+    new_parameters <- .split.bregman.low.rank.plus.sparse.update.parameters(parameters, options)
     
     # Check convergence of S, L
-    if (frobenius.norm(parameters$L) > 0) {
-      diff <- frobenius.norm(new_parameters$S - parameters$S) / frobenius.norm(parameters$S) + 
-        frobenius.norm(new_parameters$L - parameters$L) / frobenius.norm(parameters$L)
+    if(sum(new_parameters$S) == 0) {
+      parameters$S <- diag(p)
+      parameters$termcode <- -2
+      break()
+    }
+    if ((matrixcalc::frobenius.norm(parameters$L) > 0)) {
+      diff <- matrixcalc::frobenius.norm(new_parameters$S - parameters$S) / matrixcalc::frobenius.norm(parameters$S) +
+        matrixcalc::frobenius.norm(new_parameters$L - parameters$L) / matrixcalc::frobenius.norm(parameters$L)
     } else {
-      diff <- frobenius.norm(new_parameters$S - parameters$S) / frobenius.norm(parameters$S) + 
-        frobenius.norm(new_parameters$L - parameters$L)
+      diff <- matrixcalc::frobenius.norm(new_parameters$S - parameters$S) / matrixcalc::frobenius.norm(parameters$S) +
+        matrixcalc::frobenius.norm(new_parameters$L - parameters$L)
     }
     diffs <- c(diffs, diff)
     parameters <- new_parameters
     if (diff < tol) {
+      parameters$termcode <- 0
       break()
     }
   }
   
-  parameters$iter <-i 
+  parameters$iter <-i
   parameters$diffs <- diffs
   
   parameters
--- RCode/test_split_bregman_lrps.R
+++ RCode/test_split_bregman_lrps.R
@@ -1,25 +1,23 @@
-source("./generate_data.R")
-source("./SplitBregmanMethod/split_bregman_lrps.R")
-require(ggplot2)
+rm(list=ls())
+source("./split_bregman_lrps.R")
+library(ggplot2)
+library(R.matlab)
 
-set.seed(3)
-N <- 10000
-nO <- 40 # 40 Observed Variables
-nL <- 3 # 3 Latent variables
-sp_O_L <- 0.7 # Sparsity of graph O -> Latent
-sp_O <- 0.01 # Sparsitiy of observed DAG
-d <- generate_data(nO, nL, sp_O_L, sp_O, N)
-X <- d$data[,d$obs]
-for (i in 1:nO) {
-  v <- X[,]
-  X[,i] <- (v - mean(v)) / sd(v)
-}
-Sigma <- cov(X)
-# True moralised matrix
-moramat = get.adjacency(d$ug, sparse=FALSE)[d$obs, d$obs]
+# Load a toy dataset.
+# p = 32, n = 3000. 
+toy.data <- readMat("./test_data.mat")
+moramat <- toy.data$SX # True precision matrix
+moramat <- 1 * (moramat !=0) # Here we look only the sparsity pattern.
+moramat <- moramat - diag(diag(moramat)) # Remove the diagonal entries
+Lmle <- toy.data$LXmle # This is an estimate of the latent piece obtained by computing the MLE with 5.10^5 samples.
+# The true rank of L is 4.
+
+# Compute the covariance matrix
+Sigma <- cor(toy.data$X)
 
 # Look at the precision recall curves for a range of values of gamma / lambda
 gammas <- c(0.2, 0.5, 0.7)
+# We compute the full path for multiple values of Gamma. A warm stat is used.
 lambdas <- 1.1**(c(-50:10))
 results <- matrix(NA, ncol=6)
 
@@ -30,9 +28,11 @@ for (gamma in gammas) {
   for (lambda in lambdas) {
     l1 <- lambda * gamma
     l2 <- lambda * (1 - gamma)
-    fit <- split.bregman.low.rank.plus.sparse(Sigma, l1, l2, init = fit, maxiter = 10000)
-    # Value of the objective function as a function of the iterations
-    #plot(fit$objs)
+    # Reuse the previous fit as initial value.
+    fit <- fit.low.rank.plus.sparse(Sigma, l1, l2, init = fit, maxiter = 10000, mu=0.001, tol = 1e-7)
+    if(fit$termcode == -2) {
+      break()
+    }
     rank <- sum(eigen(fit$L)$val > 1e-6)
     S <- fit$S - diag(diag(fit$S))
     S <- S != 0
@@ -51,15 +51,12 @@ df.results <- as.data.frame(results[2:nrow(results),])
 names(df.results) <- c("Lambda", "Gamma", "Precision", "Recall", "Rank", "Sparsity")
 idx <- is.nan(df.results$Precision)
 df.results <- df.results[!idx,]
-# Plot the Precision / Recall curve
+# Plot the Precision / Recall curves. One curve per value of Gamma
 ggplot(df.results, aes(x=Recall, y=Precision)) + geom_point(aes(colour=as.factor(Gamma))) +
   facet_wrap(~Gamma)
-# Plot the rank as function of Lambda, Gamma
+# Plot the rank as function of Lambda, Gamma.
 ggplot(df.results, aes(x=Lambda, y=Rank)) + geom_point(aes(colour=as.factor(Gamma))) +
   facet_wrap(~Gamma)
 # Plot the sparsity as a function of Lambda, Gamma
 ggplot(df.results, aes(x=Lambda, y=log(Sparsity))) + geom_point(aes(colour=as.factor(Gamma))) +
   facet_wrap(~Gamma)
-
-# Both the graph structure and rank of the latent piece are correctly estimated for
-# gamma = 0.5
